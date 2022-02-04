7d0a142d315356f5f0b6a691fa7784140833112d
wisdomfire <468179220@qq.com>
2022-02-05 01:08:26 +0800

    1.02

---
+++ src/views/rights/Rights.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-table :data="rightslist" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="权限名称" prop="authName"></el-table-column>
+        <el-table-column label="路径" prop="path"></el-table-column>
+        <el-table-column label="权限等级" prop="level">
+        <template v-slot="scope">
+          <el-tag v-if="scope.row.level==='0'">一级</el-tag>
+          <el-tag type="success" v-else-if="scope.row.level==='1'">二级</el-tag>
+          <el-tag type="warning" v-else-if="scope.row.level==='2'">三级</el-tag>
+        </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rightslist: [],
+    };
+  },
+  created() {
+    this.getrightslist();
+  },
+  methods: {
+    async getrightslist() {
+      const { data: res } = await this.request.get("/rights/list");
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取权限列表失败");
+      }
+      this.rightslist = res.data;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
