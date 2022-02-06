69f2ab8ecbb3fb53c92cc59209d18edb5d7a2f1b
wisdomfire <468179220@qq.com>
2022-02-06 21:55:24 +0800

    1.01

---
+++ src/elementui/index.js
@@ -0,0 +1,42 @@
+import {
+  Form, FormItem, Button, Input, Message,
+  Upload, Checkbox, Container, Header,
+  Aside, Main, Menu, Submenu, MenuItemGroup,
+  MenuItem, Breadcrumb, BreadcrumbItem, Card,
+  Row, Col, Table, TableColumn, Tooltip, Switch,
+  Pagination, Dialog, MessageBox
+} from 'element-ui'
+
+
+const element = {
+  install: function (Vue) {
+    Vue.use(Form)
+    Vue.use(FormItem)
+    Vue.use(Button)
+    Vue.use(Input)
+    Vue.prototype.$message = Message
+    Vue.prototype.$confirm = MessageBox.confirm
+    Vue.use(Upload)
+    Vue.use(Checkbox)
+    Vue.use(Container)
+    Vue.use(Header)
+    Vue.use(Aside)
+    Vue.use(Main)
+    Vue.use(Menu)
+    Vue.use(Submenu)
+    Vue.use(MenuItemGroup)
+    Vue.use(MenuItem)
+    Vue.use(Breadcrumb)
+    Vue.use(BreadcrumbItem)
+    Vue.use(Card)
+    Vue.use(Row)
+    Vue.use(Col)
+    Vue.use(Table)
+    Vue.use(TableColumn)
+    Vue.use(Switch)
+    Vue.use(Tooltip)
+    Vue.use(Pagination)
+    Vue.use(Dialog)
+  }
+}
+export default element
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import request from "./utils/request.js";
+import "./assets/css/index.css"
+
+import element from "./elementui/index"
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(element)
+Vue.prototype.request = request
+Vue.config.productionTip = false
+
+new Vue({
+
+  router,
+  render: h => h(App)
+}).$mount('#app')
